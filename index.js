61a5b6b33657953f250e5a4fa97663b7f74c2754
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-11-29 17:07:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const mainForTitles = document.querySelector("main");
-const fetchBooks = () => {
-  fetch("https://anapioficeandfire.com/api/books").then(res => res.json()).then(data => renderBooks(data)).catch(err=>console.log(err));
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books").then(res => res.json()).then(data => renderBooks(data));
 }
 
 function renderBooks(data) {
-  console.log(data);
   for(let i = 0; i < data.length; i++){
     let h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${data[i].name}</h2>`;
